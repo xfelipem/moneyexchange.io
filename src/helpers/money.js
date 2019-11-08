ea36e44a93c1b251734a4345c97bb59ca295cea7
Felipe Medina <medina.xavier.felipe@gmail.com>
2019-11-08 11:30:46 -0300

    Removed dinero.js and decimals.js as dependencies

--- src/helpers/money.js
+++ src/helpers/money.js
@@ -1,75 +1,44 @@
-import Dinero from 'dinero.js';
-import Decimal from 'decimal.js';
-import config from '../mocks/configuration'
-
-const { DECIMAL_PRECISION, DEFAULT_CURRENCY } = config.exchange
 
 class MoneyHelper {
-  constructor(baseCurrency = DEFAULT_CURRENCY, decimalPrecision = DECIMAL_PRECISION) {
-    // Dinero.defaultAmount = 0
-    Dinero.defaultCurrency = baseCurrency
-    Dinero.defaultPrecision = decimalPrecision
-
-    Decimal.set({ rounding: 7 });
-
-    this.Dinero = Dinero;
-  }
-
   convert(baseAmount, targetCurrency, rates) {
     const targetCurrencyRate = { ...rates }[targetCurrency];
+
     const integerBaseAmount = this.transformToInteger(baseAmount);
     const integerTargetCurrencyRate = this.transformToInteger(targetCurrencyRate);
-    const dineroBaseAmount = Dinero({
-      amount: integerBaseAmount.integer,
-      precision: integerBaseAmount.precision
-    });
-    const dineroTargetCurrencyRate = Dinero({
-      amount: integerTargetCurrencyRate.integer,
-      precision: integerTargetCurrencyRate.precision
-    });
 
-    const [dineroBaseValue, dineroTargetRate] = Dinero.normalizePrecision([
-      dineroBaseAmount,
-      dineroTargetCurrencyRate
-    ]);
+    const exchange = integerBaseAmount.amount * integerTargetCurrencyRate.amount;
+    const decimalPrecision = integerBaseAmount.decimalPrecision + integerTargetCurrencyRate.decimalPrecision;
+    const unitPrecision = `${exchange}`.length - decimalPrecision;
 
-    const exchange = dineroBaseValue.getAmount() * dineroTargetRate.getAmount();
-    const precision = 10 ** (dineroBaseValue.getPrecision()-1);
+    const precision = unitPrecision + decimalPrecision;
 
-    const exchangedValue = Dinero({
+    const exchangedValue = {
       amount: exchange,
       currency: targetCurrency,
-      precision: dineroBaseValue.getPrecision()
-    });
-
-    console.log({
-      exchange,
       precision,
-      base: dineroBaseValue.toObject(),
-      exchangedValue: exchangedValue.toObject(),
-      precisionAmount: integerBaseAmount.precision,
-      precisionTarget: integerTargetCurrencyRate.precision,
-      targetCurrencyRate
-    });
+      decimalPrecision,
+      unitPrecision
+    };
 
-    return exchangedValue
-      .toObject();
+    return exchangedValue;
   }
 
   transformToInteger(number) {
     const normalizeDecimalCharacter = parseFloat(`${number}`.replace(',', '.'));
+    const stringNumber = normalizeDecimalCharacter.toString();
 
-    console.log({ normalizeDecimalCharacter });
+    const [units, decimals] = stringNumber.split('.');
+    const unitPrecision = units.length;
+    const decimalPrecision = decimals ? decimals.length : 0;
 
-    const decimalNumber = new Decimal(normalizeDecimalCharacter);
-    const [digit0, digit1] = decimalNumber.d;
-
-    console.log({ decimalPrecision: decimalNumber.precision(), precision: digit1 ? `${digit1}`.length : 1 });
-
+    const precision = unitPrecision + decimalPrecision;
+    const amount = parseInt(`${units}${decimals}`);
 
     return {
-      integer: digit1 ? parseInt(`${digit0}${digit1}`) : digit0,
-      precision: decimalNumber.precision()
+      amount,
+      precision,
+      unitPrecision,
+      decimalPrecision
     };
   }
 }
