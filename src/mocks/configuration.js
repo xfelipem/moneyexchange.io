9e766824433b73baf6abe553dc66866a02d3b017
Felipe Medina <medina.xavier.felipe@gmail.com>
2019-11-08 09:24:55 -0300

    Api mock configuration added

--- src/mocks/configuration.js
+++ src/mocks/configuration.js
@@ -1,8 +1,9 @@
 export default {
   exchange: {
-    DEFAULT_CURRENCY: 'EUR',
-    DECIMAL_PRECISION: 4,
+    API_ACCESS_KEY: '33b23d6e01efe285daf21f65e1124757',
     API_RESPONSE_LIFE: 10,
-    API_RESPONSE_LIFE_UNIT: 'minutes'
+    API_RESPONSE_LIFE_UNIT: 'minutes',
+    DEFAULT_CURRENCY: 'EUR',
+    DECIMAL_PRECISION: 4
   }
 };
